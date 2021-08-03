77312cdc5ca59ce14e0a3f04bd1fa1f7ba589a6d
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-08-03 20:51:28 +0900

    Create first.dart

---
+++ Flutter/flutter app8/first.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FirstPage'),
+      ),
+    );
+  }
+}

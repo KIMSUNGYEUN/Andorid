86fae4c9da4a59817eb2a7a5ffb03d395f0fd602
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-08-16 23:16:34 +0900

    Update main.dart

--- Flutter/vote_application/main.dart
+++ Flutter/vote_application/main.dart
@@ -1,1 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Widget sample = Container(
+    child: ListView(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      children: [
+        Container(
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: ListTile(
+            title: Text('C'),
+            trailing: Text('20'),
+          ),
+        ),
+
+        Container(
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: ListTile(
+            title: Text('C++'),
+            trailing: Text('20'),
+          ),
+        ),
+
+        Container(
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: ListTile(
+            title: Text('C#'),
+            trailing: Text('20'),
+          ),
+        )
+      ],
+    ),
+  ),
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('votes'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('list').snapshots(),
+        builder: (context, snapshot){
+          if(!snapshot.hasData) return LinearProgressIndicator();
+          return ListView(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            children: snapshot.data.docs.map((data) => buildListItem(context, data)).toList(),
+          )
+        },
+      )
+    );
+  }
+
+  Widget buildListItem(BuildContext context, DocumentSnapshot document){
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: ListTile(
+        title: Text(document.data() ['name']),
+        trailing: Text(document.data()['votes'].toString()),
+      ),
+    );
+  }
+}

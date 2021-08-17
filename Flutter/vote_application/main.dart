aa1715d6afd9072c579713eace5367638e6b649f
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-08-17 19:14:09 +0900

    Update main.dart

--- Flutter/vote_application/main.dart
+++ Flutter/vote_application/main.dart
@@ -1,46 +1,122 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_firebase/chart.dart';
 
-class Chart extends StatefulWidget {
-  const Chart({Key? key}) : super(key: key);
-
-  @override
-  _ChartState createState() => _ChartState();
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-class _ChartState extends State<Chart> {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Widget sample = Container(
+    child: ListView(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      children: [
+        Container(
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: ListTile(
+            title: Text('C'),
+            trailing: Text('20'),
+          ),
+        ),
+
+        Container(
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: ListTile(
+            title: Text('C++'),
+            trailing: Text('20'),
+          ),
+        ),
+
+        Container(
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: ListTile(
+            title: Text('C#'),
+            trailing: Text('20'),
+          ),
+        )
+      ],
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chart'),
+        title: Text('Votes'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.show_chart), 
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Chart())
+                );
+              },
+          )
+        ],
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance.collection('list').snapshots(),
         builder: (context, snapshot){
           if(!snapshot.hasData) return LinearProgressIndicator();
-          return SfCartesianChart(
-            primaryXAxis: CategoryAxis(),
-            title: ChartTitle(text: 'Programming language'),
-            tooltipBehavior: TooltipBehavior(enable: true),
-            series: <ChartSeries<ProgrammingData, String>>[
-              ColumnSeries<ProgrammingData, String>(
-                color: Colors.deepPurpleAccent,
-                dataSource: snapshot.data.docs.map((chart) => )
-              )
-            ],
+          return ListView(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            children: snapshot.data!.docs.map((data) => buildListItem(context, data)).toList(),
           );
         },
       )
     );
   }
-}
 
-class ProgrammingData {
-  ProgrammingData(this.name, this.votes);
-
-  final String name;
-  final int votes;
+  Widget buildListItem(BuildContext context, DocumentSnapshot document){
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: ListTile(
+        title: Text((document.data()as dynamic)['name']),
+        trailing: Text((document.data()as dynamic)['votes'].toString()),
+        onTap: () => document.reference.update({'votes': FieldValue.increment(1)},)
+      ),
+    );
+  }
 }

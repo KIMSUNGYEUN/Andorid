fa0985de8ad1800870ec093a526785ba4160e81d
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-08-23 20:25:59 +0900

    Create screen_quiz.dart

---
+++ Flutter/Quiz/lib/screen/screen_quiz.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:quize_app_test/modle/model_quiz.dart';
+
+class QuizScreen extends StatefulWidget {
+  List<Quiz>? quizs;
+  QuizScreen({this.quizs});
+  //const QuizScreen({ Key? key }) : super(key: key);
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  List<int> _answers = [-1, -1, -1];
+  List<bool> _answerState = [false, false, false, false];
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.deepPurple),
+            ),
+            width: width * 0.85,
+            height: height * 0.5,
+          ),
+        ),
+      )
+    );
+  }
+}

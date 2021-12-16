e03d8643abc8567092d133cb823fc7d53b6ad3f3
kimsy030 <201605ksy@dsm.hs.kr>
2021-12-16 20:26:21 +0900

    Update model_quiz.dart

--- Flutter/Quiz/lib/modle/model_quiz.dart
+++ Flutter/Quiz/lib/modle/model_quiz.dart
@@ -3,10 +3,16 @@ class Quiz {
   List<String>? candidates;
   int? answer;
 
-  Quiz({this.title, this.candidates, this.answer});
+  Quiz({required this.title,required this.candidates,required this.answer});
 
   Quiz.fromMap(Map<String, dynamic> map)
       : title = map['title'],
         candidates = map['candidates'],
         answer = map['answer'];
+  
+  Quiz.fromJson(Map<String, dynamic> json)
+    : title = json['title'],
+    candidates = json['body'].toString().split('/'),
+    answer = json['answer'];
+
 }
